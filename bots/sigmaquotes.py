4f8c0a44eb53882115cd59eac28501f15aac9bb2
Veresek <filipowskimedia@gmail.com>
2026-04-11 13:53:12 +0200

    feat: implement scheduled balance announcements and enhance manifesto handling

--- bots/sigmaquotes.py
+++ bots/sigmaquotes.py
@@ -3,20 +3,24 @@ import aiohttp
 from google import genai
 from dotenv import load_dotenv
 import os
+from discord.ext import tasks
+import datetime
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-raw_id = os.getenv('CWEL_MANIFESTO_ID')
-
 TOKEN_DISCORD = os.getenv('SIGMA_TOKEN')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-CWEL_MANIFESTO_ID = int(raw_id) if raw_id and raw_id.isdigit() else None
-
 client = discord.Client(intents=intents)
 gemini = genai.Client(api_key=GEMINI_API_KEY)
+
+AUTO_THREAD_CHANNELS = [1488573012801486989, 1479407068661288990, 1460589191732924567, 1491376568931586218, 1466657754616827914, 1488562360229822536] 
+BALANCE_ANNOUNCE_CHANNEL_ID = 1455924587912233109
+CWEL_MANIFESTO_ID = 1470102957042303057
+
+time_to_announce = datetime.time(hour=10, minute=0, second=0, microsecond=0)
 
 cwel_manifesto = ""
 
@@ -27,10 +31,16 @@ def generate_content(prompt):
     )
     return response.text
 
+def is_last_day_of_month():
+    today = datetime.date.today()
+    next_day = today.replace(day=today.day + 1)
+    return next_day.month != today.month
+
 async def cwel_manifesto_scraper():
     if CWEL_MANIFESTO_ID is None:
         print("Error: CWEL_MANIFESTO_ID is not set in .env")
         return
+        
 
     channel = client.get_channel(CWEL_MANIFESTO_ID)
 
@@ -70,6 +80,8 @@ async def on_ready():
     print(f'Logged in as {client.user}')
     await cwel_manifesto_scraper()
     print("Manifest loaded successfully")
+    if not announce_balance.is_running():
+        announce_balance.start()
 
 @client.event
 async def on_message(message):
@@ -85,7 +97,7 @@ async def on_message(message):
         else:
             await message.channel.send("Niestety manifest jest pusty lub nie został wczytany.")
         return
-
+    # Dodawanie cytatu do bazy danych, jeśli bot został wspomniany i wiadomość jest odpowiedzią na inną wiadomość
     if client.user and client.user.mentioned_in(message):
         print(f"Received mention, length: {len(message.content)}")
 
@@ -111,7 +123,7 @@ async def on_message(message):
                 print(f"Error during quote saving: {e}")
                 await message.reply("Nie udało się pobrać wiadomości lub połączyć z bazą danych.")
                 return
-
+        # Jeśli wiadomość nie jest odpowiedzią, ale nadal wspomina bota, generujemy odpowiedź
         if len(message.content) > 10:
             response = generate_content(message.content)
             if response and response.strip():
@@ -120,15 +132,34 @@ async def on_message(message):
                 await message.reply("Nie wiem co powiedzieć, zatkało mnie.")
         else:
             await message.reply("Szkoda prądu na takie gówno")
+    # Automatyczne tworzenie wątku, jeśli wiadomość została wysłana na określonych kanałach i nie jest od bota
+    if message.channel.id in AUTO_THREAD_CHANNELS and not message.author.bot:
+        thread_name = message.content
+        try:
+            await message.create_thread(name=thread_name)
 
+        except Exception as e:
+            print(f"Error creating thread: {e}")
 @client.event
 async def on_message_edit(before, after):
+    # Aktualizuj manifest, jeśli wiadomość została edytowana w kanale manifestu
     if after.channel.id == CWEL_MANIFESTO_ID:
         await cwel_manifesto_scraper()
 
 @client.event
 async def on_message_delete(message):
+    # Aktualizuj manifest, jeśli wiadomość została usunięta w kanale manifestu
     if message.channel.id == CWEL_MANIFESTO_ID:
         await cwel_manifesto_scraper()
+
+@tasks.loop(time=time_to_announce)
+async def announce_balance():
+    print("Running scheduled balance announcement task...")
+    channel = client.get_channel(BALANCE_ANNOUNCE_CHANNEL_ID)
+    if channel and isinstance(channel, discord.TextChannel) and is_last_day_of_month():
+        minio_id = "339884510089052160" 
+        veresek_id = "986324349067874326"
+        await channel.send(f"<@{minio_id}> <@{veresek_id}> Dzisiaj ostatni dzień miesiąca, zapraszam do rozliczenia za przewinienia! 🗿")
+        
 
 client.run(str(TOKEN_DISCORD))
