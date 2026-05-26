273692d4587a2f2b3949b5c27ec106d3288f7ddd
Kacper Filipowski <filipowskipraca@gmail.com>
2026-05-26 14:50:11 +0200

    dawda

--- bots/sigmaquotes.py
+++ bots/sigmaquotes.py
@@ -122,7 +122,7 @@ class SigmaQuotesBot(discord.Client):
                 kwargs['response_mime_type'] = "application/json"
                 
             return self.gemini.models.generate_content(
-                model="gemini-3.1-flash-lite-preview",
+                model="gemini-3.1-flash-lite",
                 contents=prompt,
                 config=genai.types.GenerateContentConfig(**kwargs)
             )
